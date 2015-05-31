3c273be2ed39a5aae0a690659e99cf8ce15e9fa8
Israel Fonseca <israel.bgf@gmail.com>
2015-05-31 00:09:27 -0300

    Layout não quebra mais em telas pequenas.

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(function(){
 })
 
 function parseArquivoDeRelatos(arquivo){
-    var tokens, data, titulo, relatoBianca, relatoIsrael;
+    var tokens, data, titulo, relatoBianca, relatoIsrael, imagem;
     var contador = 0
 
     for(let linha of arquivo.split('\n')){
@@ -18,40 +18,48 @@ function parseArquivoDeRelatos(arquivo){
             tokens = linha.split(' - ')
             data = tokens[0]
             titulo = tokens[1]
+            imagem = titulo.substring(titulo.indexOf('{') + 1, titulo.indexOf('}'))
+            titulo = titulo.substring(0, titulo.indexOf('{'))
         }
         else if(contador == 2)
             relatoBianca = linha
         else if(contador == 3) {
             relatoIsrael = linha
             contador = 0
-            $('#timeline').append(gerarHtmlDoRelato(data, titulo, relatoBianca, relatoIsrael))
+            $('#timeline').append(gerarHtmlDoRelato(data, titulo, relatoBianca, relatoIsrael, imagem))
         }
     }
 }
 
-function gerarHtmlDoRelato(data, titulo, bianca, israel){
+function gerarHtmlDoRelato(data, titulo, bianca, israel, imagem){
     let relato = `
-    <div style="margin-top: 50px">
-        <div class="row" style="margin-bottom: 15px">
+    <div style="margin-top: 20px;">
+        <div>
             <h5>
                 <div style="background-color: #fff; border-radius: 10px; padding: 10px; width: 300px; margin: auto">
                     <strong>${data}</strong><br>${titulo}
                 </div>
             </h5>
         </div>
-        <div class="row">
-            <img style="float: left; margin-top: 30px" src="img/bianca_avatar.png" class="avatar-frame">
-            <blockquote style="float: left; width: 80%; margin: 0; margin-left: 30px" class="balao-bianca">
+        <div>
+            <div style="float: left; width: 100px">
+                <img src="img/bianca_avatar.png" class="avatar-frame">
+            </div>
+            <p class="chat-bubble left" style="margin-left: 130px">
                 ${bianca}
-            </blockquote>
+            </p>
         </div>
-        <div class="row" style="margin-top: 30px">
-            <img style="border-radius: 10px;" class="borda" src="img/corrida.png" width="280">
-            <img style="float: right; margin-top: 20px; margin-left: 30px" src="img/israel_avatar.png" class="avatar-frame">
-            <blockquote style="float: right; width: 50%; margin: 0; margin-left: 30px" class="balao-israel">
+        <div>
+            <div style="float: right; width: 100px">
+                <img src="img/israel_avatar.png" class="avatar-frame">
+            </div>
+            <p class="chat-bubble right" style="margin-right: 130px">
                 ${israel}
-            </blockquote>
-        </div>
+            </p>
+         </div>
+         <div>
+             <img src="img/${imagem}" class="imagem">
+         </div>
     </div>
     `;
     return relato;
