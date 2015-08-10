e23fbbed9c8a06ee07ab9a11ca7a74efcc0e7381
Israel Fonseca <israel.bgf@gmail.com>
2015-08-09 21:56:17 -0300

    Corrigido URL

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from gateways.relato_gateway import RelatoGatewayArquivoTXT
 
 
 def index(request):
-    gateway = RelatoGatewayArquivoTXT(open('relatos/relatos.html'))
-    relatos = gateway.listar()
+    with open('relatos/relatos.html') as arquivo:
+        gateway = RelatoGatewayArquivoTXT(arquivo)
+        relatos = gateway.listar()
 
     return render(request, 'blog/index.html', {
         'relatos': relatos
